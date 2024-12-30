7c3d020d7400cc8c4aa52f65d33da911aabf1dbb
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-12-31 00:26:22 +0600

    User Input From A Form

--- Project 17 User Input From A Form/app.js
+++ Project 17 User Input From A Form/app.js
@@ -1,2 +1,33 @@
 //Project 17 User Input From A Form
 
+
+window.onload = function(){
+    main()
+}
+
+function main(){
+    const reserBtn = document.getElementById('reset-btn')
+    const submitBtn = document.getElementById('submit-btn')
+    const nameInp = document.getElementById('name-inp')
+    const nameOutput = document.getElementById('name-output')
+    const resultBody = document.getElementById('result-body')
+
+    resultBody.style.display = 'none'
+
+    reserBtn.addEventListener('click', function(){
+        nameInp.value = ''
+        resultBody.style.display = 'none'
+
+    })
+    submitBtn.addEventListener('click', function(){
+        const name = nameInp.value
+
+        if(!name){
+            alert('Please provide a valid name')
+        } else{
+            resultBody.style.display = 'block'
+            nameOutput.innerHTML = name
+            nameInp.value = ''
+        }
+    })
+}
