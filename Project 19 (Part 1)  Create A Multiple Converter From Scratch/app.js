66c32f9da1794d067364c2029cbfa49fdf14904a
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-01-04 01:09:28 +0600

    Create A Multiple Converter From Scratch

--- Project 19 (Part 1)  Create A Multiple Converter From Scratch/app.js
+++ Project 19 (Part 1)  Create A Multiple Converter From Scratch/app.js
@@ -6,8 +6,52 @@ window.onload = function(){
 }
 
 //globals
-let result = 0
+
+const converter = {
+    area: {
+        name: 'Area'
+    },
+    mass: {
+        name: 'Mass'
+    },
+    length: {
+        name: 'Length'
+    },
+    volume: {
+        name: 'Volume'
+    },
+    time: {
+        name: 'Time'
+    },
+    dataTransferRate: {
+        name: 'Data Transfer Rate'
+    },
+    pressure: {
+        name: 'Pressure'
+    }
+
+}
 
 function main(){
-   
+   const categorySelect = document.getElementById('category-select')
+
+   const converterKeys = Object.keys(converter).sort()
+   removeAllChild(categorySelect)
+   converterKeys.forEach((item)=>{
+    addOption(categorySelect,{value: item, text: converter[item].name})
+   })
+}
+
+function addOption(parent, option){
+    const opt = document.createElement('option')
+    opt.setAttribute('value', option.value)
+    opt.innerText = option.text
+
+    parent.appendChild(opt)
+}
+
+function removeAllChild(parent){
+    while(parent.firstChild){
+        parent.firstChild.remove()
+    }
 }
