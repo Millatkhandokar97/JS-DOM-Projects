842233e7c4493cf5d808eaa577238197f00d1304
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2025-01-05 00:53:25 +0600

    Create A Multiple Converter From Scratch

--- Project 19 (Part 1)  Create A Multiple Converter From Scratch/app.js
+++ Project 19 (Part 1)  Create A Multiple Converter From Scratch/app.js
@@ -9,19 +9,46 @@ window.onload = function(){
 
 const converter = {
     area: {
-        name: 'Area'
+        name: 'Area',
+        units: {
+            squareKm: 'Square Kilometer',
+            squareM: 'Square Meter',
+            squareMile: 'Square Mile',
+            squareYard: 'Square Yard',
+            squareFoot: 'Square Foot'
+        }
     },
     mass: {
-        name: 'Mass'
+        name: 'Mass',
+        units: {
+            tonne: 'Tonne',
+            kilogeam: 'Kilogram',
+            gram: 'Gram',
+            milligram: 'Milligram'
+        }
     },
     length: {
-        name: 'Length'
+        name: 'Length',
+        units: {
+            kilometer: 'Kilometer',
+            meter: 'Meter',
+            centimeter: 'Centimeter',
+            millimeter: 'Millimeter'
+        }
     },
     volume: {
         name: 'Volume'
     },
     time: {
-        name: 'Time'
+        name: 'Time',
+        units: {
+            second: 'Second',
+            minute: 'Minute',
+            hour: 'Hour',
+            day: 'Day',
+            week: 'Week',
+            month: 'Month'
+        }
     },
     dataTransferRate: {
         name: 'Data Transfer Rate'
@@ -34,12 +61,21 @@ const converter = {
 
 function main(){
    const categorySelect = document.getElementById('category-select')
+   const leftSelect = document.getElementById('left-select')
+   const righrSelect = document.getElementById('right-select')
 
    const converterKeys = Object.keys(converter).sort()
    removeAllChild(categorySelect)
    converterKeys.forEach((item)=>{
     addOption(categorySelect,{value: item, text: converter[item].name})
    })
+
+   // handle left select
+   const converterName = categorySelect.value
+   removeAllChild(leftSelect)
+
+   // handle right select
+   removeAllChild(righrSelect)
 }
 
 function addOption(parent, option){
